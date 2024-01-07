4dc32a2c9c44d4d36b9104db0a5fc4932d6ae322
Saaoz <bastie_nb1998@hotmail.fr>
2024-01-07 20:10:35 +0100

    [FEATURE] Modification de la page main pour prendre en compte les deux composants test et donc commit avant de faire un test pour garder le patern

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react"
+import SearchBar from "../components/common/searchBar";
+import ItemList from "../components/common/ItemList";
 
-function Main() {
-  return (
-    <div>Main</div>
-  )
-}
+const data = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+    { id: 4, name: 'Item 4' },
+    { id: 5, name: 'Item 5' },
+    { id: 6, name: 'Item 6' },
+];
 
-export default Main
+const MainPage: React.FC = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (term: string) => {
+        console.log('REcherche effectué avec le terme :', term);
+        setSearchTerm(term);
+    };
+
+    return (
+        <div>
+            <h1>Page d'Accueil</h1>
+            <SearchBar onSearch={handleSearch} />
+            <ItemList items={data} />
+        </div>
+    );
+};
+
+export default MainPage;
