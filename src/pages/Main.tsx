3b1f84362370aa21a08d89bab795cd08feb1d2c6
Saaoz <bastie_nb1998@hotmail.fr>
2024-01-09 16:23:57 +0100

    [FEATURE] Update de la searchbar avec une recherche avec 3>= caractère plus besoin de forcément cliquer sur recherche mais le bouton restera disponible + ajout d'un filtre pour la compréhesion / fonctionne en typescript

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,27 +3,51 @@ import SearchBar from "../components/common/SearchBar";
 import ItemList from "../components/common/ItemList";
 
 const data = [
-    { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-    { id: 3, name: 'Item 3' },
-    { id: 4, name: 'Item 4' },
-    { id: 5, name: 'Item 5' },
-    { id: 6, name: 'Item 6' },
+    { id: 1, name: 'Valorant' },
+    { id: 2, name: 'Far cry 6' },
+    { id: 3, name: 'PUBG' },
+    { id: 4, name: 'Tarkov' },
+    { id: 5, name: 'React' },
+    { id: 6, name: 'Typescript' },
+    { id: 7, name: 'Far cry 2' },
+    { id: 8, name: 'Far cry 5' },
 ];
+
+interface Item {
+    id: number;
+    name: string;
+  }
 
 const MainPage: React.FC = () => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [filteredData, setFilteredData] = useState<Item[]>([]);
 
     const handleSearch = (term: string) => {
-        console.log('REcherche effectué avec le terme :', term);
+        // console.log('REcherche effectué avec le terme :', term);
         setSearchTerm(term);
+
+        if (term.length >= 3) {
+            const filtered = data.filter(item => item.name.toLowerCase().includes(term.toLowerCase()));
+            setFilteredData(filtered);
+        } else {
+            setFilteredData([]);
+        }
     };
+
+    const handleChange = (value: string) => {
+        if (value.length >= 3) {
+            const suggestions = data.filter(item => item.name.toLowerCase().includes(value.toLowerCase()));
+            setFilteredData(suggestions);
+        } else {
+            setFilteredData([]);
+        }
+    };  
 
     return (
         <div>
             <h1>Page d'Accueil</h1>
-            <SearchBar onSearch={handleSearch} />
-            <ItemList items={data} />
+            <SearchBar onSearch={handleSearch} onChange={handleChange} />
+            {filteredData.length > 0 && <ItemList items={filteredData} />}
         </div>
     );
 };
