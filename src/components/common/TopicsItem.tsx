58e64b1dfc8290cee88f11525da7d5ab5f348385
Saaoz <bastie_nb1998@hotmail.fr>
2024-03-02 18:25:18 +0100

    [UPDATE] refonte quasi total du composant pour la gestion supplémentaire des count reply et date

--- src/components/common/TopicsItem.tsx
+++ src/components/common/TopicsItem.tsx
@@ -1,22 +1,25 @@
 import React from "react";
+import { TopicWithReplyDetails } from "../context/types";
+import { formatDistanceToNow, parseISO } from "date-fns";
+import { fr } from 'date-fns/locale';
 
-interface TopicsItemProps {
-    id?: number;
-    title?: string;
-    index?: number;
-}
-
-
-const TopicsItem: React.FC<TopicsItemProps> = ({id, title, index}) => {
+const TopicsItem: React.FC<TopicWithReplyDetails> = ({id, title, replyCount, lastActivity}) => {
     const handleclick = () => {
         // console.log(`Redirection vers ${to}`);
     };
 
+    const formatDate = (date:string) => {
+        const result = formatDistanceToNow(parseISO(date), {addSuffix:true, locale:fr});
+        return result.charAt(0).toUpperCase() + result.slice(1);
+    }
+
     return (
         <div className="body_body" onClick={handleclick}>
             <div className="titre_tag">
-              <h5 id={`item-${id}-${index}`} >{title}</h5>
+                <h5>{title}</h5>
             </div>
+            <div>{replyCount}</div>
+            <div>{lastActivity ? formatDate(lastActivity) : 'N/A'}</div>
         </div>
     );
 };
