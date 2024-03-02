123a7077ea0a4df652127f4d35d13ae4f97e7fb6
Saaoz <bastie_nb1998@hotmail.fr>
2024-03-02 18:28:03 +0100

    [FEATURE UPDATE ] Utilisation du nouveau fetch concernant les counts reply , modification des useEffect et donc aussi des nommages , incrémentation des tags et catégories , modification de l'affichage des topics avec l'ajout de count reply et de la date modifier avec une dépendance pour une meilleur gestion de l'affiche du dernier post

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,22 +4,35 @@ import MainMenu from "../components/common/MainMenu";
 import "../style/Main.css";
 import ListItemWithLink from "../components/common/ListItemWithLink";
 import TopicsItem from "../components/common/TopicsItem";
-import { fetchAllTopics } from "../api/Topics";
-import { Topic } from "../components/context/types";
+import { Category, Tag, TopicWithReplyDetails } from "../components/context/types";
+import { fetchTop10Category } from "../api/Category";
+import { fetchTop10Tag } from "../api/Tags";
+import { allTopTopicsWithReplyCount } from "../api/Topics";
 
 const MainPage: React.FC = () => {
-  const [topics, setTopics] = useState<Topic[]>([]);
+  const [topicsWithReplies, setTopicsWithReplies] = useState<TopicWithReplyDetails[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [tags, setTags] = useState<Tag[]>([]);
 
   useEffect(() => {
-    const getTopics = async() => {
-      try {
-        const data = await fetchAllTopics();
-        setTopics(data)
-      } catch (error) {
-        console.error(error)
-      }      
+    const getTopicsAndReplies = async () => {
+      const topics = await allTopTopicsWithReplyCount();
+      setTopicsWithReplies(topics);
     };
-    getTopics();
+
+    const loadCategories = async () => {
+      const categoryData = await fetchTop10Category();
+      setCategories(categoryData);
+    };
+
+    const loadTags = async () => {
+      const tagData = await fetchTop10Tag();
+      setTags(tagData);
+    };
+
+    getTopicsAndReplies();
+    loadCategories();
+    loadTags();
   }, []);
 
   return (
@@ -33,23 +46,29 @@ const MainPage: React.FC = () => {
             <h4>Responses</h4>
             <h4>Activité</h4>
           </div>
-          {topics.map((topic, index) => (
-            <TopicsItem key={`item-${topic.id}-${index}`} id={topic.id} title={topic.title} index={index} />
+          {topicsWithReplies.map((topic, index) => (
+            <TopicsItem 
+              key={`topic-${topic.id}-${index}`}
+              id={topic.id}
+              title={topic.title}
+              replyCount={topic.replyCount}
+              lastActivity={topic.lastActivity}
+            />
           ))}
         </div>
         <div>
-        <div className="menu top_category">
+          <div className="menu top_category">
             <h3>Top catégorie</h3>
-            {/* {categoryData.slice(0,7).map((item) => (
-              <ListItemWithLink key={item.id} name={item.name} to={`/`} />
-            ))} */}
-        </div>
-        <div className="menu top_tags">
+            {categories.map((category, index) => (
+              <ListItemWithLink key={`category-${category.id}-${index}`} name={category.name} to={`/`} index={index} />
+            ))}
+          </div>
+          <div className="menu top_tags">
             <h3>Top Tags</h3>
-            {/* {tagData.slice(0,7).map((item) => (
-              <ListItemWithLink key={item.id} name={item.name} to={`/`} />
-            ))} */}
-        </div>
+            {tags.map((tag, index) => (
+              <ListItemWithLink key={`tag-${tag.id}-${index}`} name={tag.name} to={`/`} index={index} />
+            ))}  
+          </div>
         </div>
       </div>
     </>
